890dd3581dc6665d681d132c8c2e891d18592f4a
JoonYong <joonyongpark99@gmail.com>
2024-11-02 15:36:05 +0900

    [FEAT] app.py에 Blueprints등록

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from app.routes.hospital_review_routes import hospital_review_bp 
+from app.routes.drive_judge_routes_faiss import drive_judge_bp  
+
+app = Flask(__name__)
+
+# Blueprint 등록
+app.register_blueprint(hospital_review_bp)
+app.register_blueprint(drive_judge_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
