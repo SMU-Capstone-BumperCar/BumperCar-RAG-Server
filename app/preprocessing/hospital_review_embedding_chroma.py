d03baa286fd3f27a81415f4922b18c678b94a6f8
JoonYong <joonyongpark99@gmail.com>
2024-11-05 08:34:12 +0900

    [FEAT] 크로마 DB 임베딩 전처리

--- app/preprocessing/hospital_review_embedding_chroma.py
+++ app/preprocessing/hospital_review_embedding_chroma.py
@@ -1,14 +1,14 @@
 import os
-from dotenv import load_dotenv
+import sys
+
 import pandas as pd
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_upstage import UpstageEmbeddings
 
-load_dotenv()
-upstage_api_key = os.getenv("UPSTAGE_API_KEY")
-if not upstage_api_key:
-    raise ValueError("UPSTAGE_API_KEY가 설정되지 않았습니다.")
+# config.py 파일이 위치한 경로를 기준으로 상위 경로를 추가
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from config import UPSTAGE_API_KEY
 
 df = pd.read_csv("app/data/hospital_review_dataset.csv")
 print("\n\nhospital_review_dataset 파일 읽기 완료")
@@ -21,7 +21,7 @@ print(f"Document 개수: {len(documents)}")
 
 # Upstage Embeddings 설정
 us_model = UpstageEmbeddings(
-    api_key=upstage_api_key,
+    api_key=UPSTAGE_API_KEY,
     model="solar-embedding-1-large"
 )
 print("임베딩 모델 설정 완료")
@@ -34,7 +34,7 @@ def add_documents_in_batches(db, documents, batch_size=5461):
         print(f"{min(i + batch_size, len(documents))}개의 문서를 데이터베이스에 추가 완료.")
 
 # Chroma Vector Store 생성 또는 기존 데이터베이스 로드
-chroma_db_path = "app/chroma_db/hospital_review_dataset2"
+chroma_db_path = "app/vector_databases/chroma_db/hospital_review_dataset2"
 
 
 print("\n새 데이터베이스를 생성합니다.")
