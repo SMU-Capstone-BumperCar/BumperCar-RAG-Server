e76cb5f68d4855d61e75f3335d1d88a1402706c5
JoonYong <joonyongpark99@gmail.com>
2024-11-05 08:52:43 +0900

    [FEAT] 엔드포인트 추가 및 서버 설정 변경

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 app.register_blueprint(hospital_review_bp)
 app.register_blueprint(drive_judge_bp)
 
+@app.route("/")
+def hello():
+    return "Hello, Flask!"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
